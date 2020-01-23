2bca05357412d1fcc596ea55fdfef12af55c5828
Manhster <51175774+Manhster@users.noreply.github.com>
2020-01-23 03:59:22 -0800

    Update config.js

--- config.js
+++ config.js
@@ -2,12 +2,12 @@ module.exports = {
   siteTitle: 'Gatsby Starter SolidState', // <title>
   manifestName: 'SolidState',
   manifestShortName: 'Landing', // max 12 characters
-  manifestStartUrl: '/',
+  manifestStartUrl: 'reikinaturalrn.com',
   manifestBackgroundColor: '#663399',
   manifestThemeColor: '#663399',
   manifestDisplay: 'standalone',
   manifestIcon: 'src/assets/img/website-icon.png',
-  pathPrefix: `/gatsby-starter-solidstate/`, // This path is subpath of your hosting https://domain/portfolio
+  pathPrefix: `/gatsby-starter-solidstate/`, // This path is subpath of your hosting https://reikinaturalrn.com
   heading: 'Reiki Natural',
   subHeading: ' Emotional & Physical Healings Transfered through Universal Energy ',
   // social
